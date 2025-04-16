99f4a85be063c204a5affade85712a1f1bd5d0ad
hyojin <gywls5184@gmail.com>
2025-04-16 21:44:00 +0900

    Revert "[FE] implement MyPage"

--- mutism/app/src/main/java/com/example/mutism/model/TagSection.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mutism.model
-
-data class TagSection(
-    val category: String,
-    val tags: List<String>,
-)
--- mutism/app/src/main/java/com/example/mutism/model/WhiteNoiseItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mutism.model
-
-data class WhiteNoiseItem(
-    val name : String,
-    val iconResId :Int,
-    val isSelected : Boolean = false
-)
